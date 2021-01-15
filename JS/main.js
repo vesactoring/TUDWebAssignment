e1ceafcc68a00447526c58c9c1a2b41d97b7a539
vesactoring <vesactoringcp@gmail.com>
2021-01-15 19:51:59 +0100

    add game rule (NOT FINISH)

--- JS/main.js
+++ JS/main.js
@@ -3,20 +3,20 @@
  */
 function initiateGame() {
     // To turn off the onlick attribute in "Start Game" button
-    var startGameButton = document.getElementById('initiate');
+    let startGameButton = document.getElementById('initiate');
     startGameButton.removeAttribute("onclick");
     startGameButton.remove();
 
     // Change the "Start game" text to "It's your turn" text
-    var turn = document.getElementById('turn');
-    var attribute = document.createElement("strong");
-    var newText = document.createTextNode("It's your turn");
+    let turn = document.getElementById('turn');
+    let attribute = document.createElement("strong");
+    let newText = document.createTextNode("It's your turn");
     attribute.appendChild(newText);
     turn.appendChild(attribute);
 
-    var tile = document.getElementsByClassName("tile");
+    let tile = document.getElementsByClassName("tile");
     console.log("test");
-    var map = [
+    let map = [
         0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
         1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
         0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
@@ -31,25 +31,29 @@ function initiateGame() {
 
     for (let i = 0; i < 100; i++) {
         // To create a div containing white pieces
-        var containerWhite = document.createElement('div');
+        let containerWhite = document.createElement('div');
         containerWhite.setAttribute("style", "position: Absolute, width: 90%; cursor: pointer");
-        if (i > 50)
+        if (i > 50) {
             containerWhite.setAttribute("id", i);
+            containerWhite.setAttribute("class", "w");
+        }
 
         // To create a div containing black pieces
-        var containerBlack = document.createElement('div');
-        containerBlack.setAttribute("style", "position: Absolute, width: 90%; cursor: pointer;");
-        containerBlack.setAttribute("onclick", "test()");
-        if (i < 50)
+        let containerBlack = document.createElement('div');
+        containerBlack.setAttribute("style", "position: Absolute, width: 90%; cursor: pointer");
+        // containerBlack.setAttribute("onclick", "test()");
+        if (i < 50) {
             containerBlack.setAttribute("id", i);
+            containerBlack.setAttribute("class", "b");
+        }
 
         // White piece (png image)
-        var piecesWhite = document.createElement("img");
+        let piecesWhite = document.createElement("img");
         piecesWhite.src = "../images/white.png";
         piecesWhite.setAttribute("style", "width: 90%; margin-top: 30%");
 
         // Black piece (png image)
-        var piecesBlack = document.createElement('img');
+        let piecesBlack = document.createElement('img');
         piecesBlack.src = "../images/black.png";
         piecesBlack.setAttribute("style", "width: 90%; margin-top: 30%");
 
@@ -64,9 +68,101 @@ function initiateGame() {
     }
 }
 
-function move() {
-    var source = document.getElementById("1");
-    var destination = document.getElementById("8_6");
-    destination.getCh
-    destination.appendChild(source);
+var state = false //false if no piece has been selected
+var returnType = false;
+var side = false; // false is white, true is black
+var currentPiece;
+var currentCell;
+
+var cells = document.getElementsByClassName("tile"); 
+for (var i = 0; i < cells.length; i++) { 
+   cells[i].onclick = function(){
+       getCell(this);
+   };
+}
+
+function getCell(that) {
+    if (!state && that.innerHTML == "") return; // if the select tile has no pieces => return, do nothing
+    if(!state) { //no piece selected
+        state = true; //piece has been selected
+        currentPiece = that.innerHTML; //get the current piece selected
+        currentCell = that; //get the current cell selection
+    } else { //else, you are moving a piece
+        if (!isValidMove(currentCell, that, side)) {
+            state = false;
+            return;
+        }
+
+        that.innerHTML = currentPiece; //Set the selected space to the piece that was grabbed
+
+        currentCell.innerHTML = ""; //remove the piece from its old location
+        state = false; //piece has been placed, so set state back to false
+        side = swap(side);
+    }
+}
+
+/**
+ * This function will return true if the move is valid, false otherwise
+ * @param {*} oldCell: the selected piece to be moved
+ * @param {*} newCell: the new cell that the piece will be in
+ */
+function isValidMove(oldCell, newCell, side) {
+    x_coordinate = [parseInt(newCell.id.substring(0, 1)), parseInt(oldCell.id.substring(0, 1))];
+    y_coordinate = [parseInt(newCell.id.substring(2,3)), oldCell.id.substring(2,3)];
+    console.log(oldCell.id);
+    console.log(newCell.id);
+    console.log(side);
+    console.log(oldCell.innerHTML.charAt(76));
+    if (!side) {
+        if (isValidMoveWhite(x_coordinate, y_coordinate, oldCell, newCell)) return true;
+    } else {
+        if (isValidMoveBlack(x_coordinate, y_coordinate, oldCell, newCell)) return true;
+    }
+    // if (x_coordinate[0] != x_coordinate[1]  &&  y_coordinate[0] > y_coordinate[1]) {
+    //     return true;
+    // }
+    return false;
+}
+
+function isValidMoveWhite(x_coordinate, y_coordinate, pieceSelected, newCell) {
+    if (pieceSelected.innerHTML.charAt(76) != 'w') return false;
+    if (x_coordinate[0] != x_coordinate[1]  &&  y_coordinate[0] > y_coordinate[1] && newCell.innerHTML == "") {
+        return true;
+    }
+    return false;
+}
+
+function isValidMoveBlack(x_coordinate, y_coordinate, pieceSelected, newCell) {
+    if (pieceSelected.innerHTML.charAt(76) != 'b') return false;
+    if (x_coordinate[0] != x_coordinate[1]  &&  y_coordinate[0] < y_coordinate[1] && newCell.innerHTML == "") {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * X captures Y
+ * @param {*} x 
+ * @param {*} y 
+ */
+function capture(x, y) {
+
+}
+
+/**
+ * To swap side
+ * @param {boolean} side currently on 
+ */
+function swap(sideSwap) {
+    if (sideSwap) return false;
+    return true;
+}
+
+/**
+ * This function check if in front of the selected piece has any other pieces
+ * @param {*} selectedPiece 
+ * @return true if there are there are, false otherwise
+ */
+function lookAhead(selectedPiece) {
+
 }
