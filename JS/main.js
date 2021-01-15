626d8d57cdf2100d631b56388db6a737b4e8b919
vesactoring <vesactoringcp@gmail.com>
2021-01-15 15:05:58 +0100

    minor change

--- JS/main.js
+++ JS/main.js
@@ -1,50 +1,72 @@
-var board = document.getElementById("DraughtBoard");
-var tile = document.getElementsByClassName("tile");
-// alert("Test");
-console.log("test");
-var map = [
-    0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
-    1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
-    0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
-    1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
-    1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
-    0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
-    1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
-];
+/**
+ * This function is used to start the game, add pieces to the board
+ */
+function initiateGame() {
+    // To turn off the onlick attribute in "Start Game" button
+    var startGameButton = document.getElementById('initiate');
+    startGameButton.removeAttribute("onclick");
+    startGameButton.remove();
 
-for (let i = 0; i < 100; i++) {
+    // Change the "Start game" text to "It's your turn" text
+    var turn = document.getElementById('turn');
+    var attribute = document.createElement("strong");
+    var newText = document.createTextNode("It's your turn");
+    attribute.appendChild(newText);
+    turn.appendChild(attribute);
 
-    // To create a div containing white pieces
-    var containerWhite = document.createElement('div');
-    containerWhite.setAttribute("style", "position: Absolute, width: 90%");
-    if (i > 50) 
-        containerWhite.setAttribute("id", i);
+    var tile = document.getElementsByClassName("tile");
+    console.log("test");
+    var map = [
+        0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+        1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+        0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+        1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+        1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+        0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+        1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+    ];
 
-    // To create a div containing black pieces
-    var containerBlack = document.createElement('div');
-    containerBlack.setAttribute("style", "position: Absolute, width: 90%");
-    if (i < 50) 
-        containerBlack.setAttribute("id", i);
+    for (let i = 0; i < 100; i++) {
+        // To create a div containing white pieces
+        var containerWhite = document.createElement('div');
+        containerWhite.setAttribute("style", "position: Absolute, width: 90%; cursor: pointer");
+        if (i > 50)
+            containerWhite.setAttribute("id", i);
 
-    // White piece (png image)
-    var piecesWhite = document.createElement("img");
-    piecesWhite.src = "../images/white.png";
-    piecesWhite.setAttribute("style", "width: 90%; margin-top: 30%"); 
+        // To create a div containing black pieces
+        var containerBlack = document.createElement('div');
+        containerBlack.setAttribute("style", "position: Absolute, width: 90%; cursor: pointer;");
+        containerBlack.setAttribute("onclick", "test()");
+        if (i < 50)
+            containerBlack.setAttribute("id", i);
 
-    // Black piece (png image)
-    var piecesBlack = document.createElement('img');
-    piecesBlack.src = "../images/black.png";
-    piecesBlack.setAttribute("style", "width: 90%; margin-top: 30%");
-    
-    // Add the pieces to the board
-    if (map[i] == 1 && i < 50) {
-        containerBlack.appendChild(piecesBlack);
-        tile[i].appendChild(containerBlack);
-    } else if (map[i] == 1 && i > 50) {
-        containerWhite.appendChild(piecesWhite);
-        tile[i].appendChild(containerWhite);
+        // White piece (png image)
+        var piecesWhite = document.createElement("img");
+        piecesWhite.src = "../images/white.png";
+        piecesWhite.setAttribute("style", "width: 90%; margin-top: 30%");
+
+        // Black piece (png image)
+        var piecesBlack = document.createElement('img');
+        piecesBlack.src = "../images/black.png";
+        piecesBlack.setAttribute("style", "width: 90%; margin-top: 30%");
+
+        // Add the pieces to the board
+        if (map[i] == 1 && i < 50) {
+            containerBlack.appendChild(piecesBlack);
+            tile[i].appendChild(containerBlack);
+        } else if (map[i] == 1 && i > 50) {
+            containerWhite.appendChild(piecesWhite);
+            tile[i].appendChild(containerWhite);
+        }
     }
+}
+
+function move() {
+    var source = document.getElementById("1");
+    var destination = document.getElementById("8_6");
+    destination.getCh
+    destination.appendChild(source);
 }
