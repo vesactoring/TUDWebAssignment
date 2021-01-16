1680ee3e37dbd0854f3bdf711476b4e90322b7dd
vesactoring <vesactoringcp@gmail.com>
2021-01-16 14:42:54 +0100

    add capturing pieces (FAR FROM FINISH)

--- JS/main.js
+++ JS/main.js
@@ -69,7 +69,9 @@ function initiateGame() {
 }
 
 var state = false //false if no piece has been selected
-var returnType = false;
+var captureAvaibality = false;
+var mustMovePiece;
+var captureCoordinate = [];
 var side = false; // false is white, true is black
 var currentPiece;
 var currentCell;
@@ -83,11 +85,17 @@ for (var i = 0; i < cells.length; i++) {
 
 function selectTile(that) {
     if (!state && that.innerHTML == "") return; // if the select tile has no pieces => return, do nothing
+
     if(!state) { //no piece selected
+        // if (captureAvaibality) {
+
+        //     return;
+        // }
         state = true; //piece has been selected
         currentPiece = that.innerHTML; //get the current piece selected
         currentCell = that; //get the current cell selection
     } else { //else, you are moving a piece
+
         if (!isValidMove(currentCell, that, side)) {
             state = false;
             return;
@@ -97,8 +105,9 @@ function selectTile(that) {
 
         currentCell.innerHTML = ""; //remove the piece from its old location
         state = false; //piece has been placed, so set state back to false
+        canCapture(that);
         side = swap(side);
-        console.log("Is there any piece around? " + lookAround(x_coordinate[0], y_coordinate[0]));
+        // console.log("Is there any piece around? " + isTherePieceAround(that));
         // Change status line
         changeStatusLine();
     }
@@ -122,6 +131,7 @@ function isValidMove(oldCell, newCell, side) {
     console.log(newCell.id);
     console.log(side);
     console.log(oldCell.innerHTML.charAt(76));
+    // console.log(canCapture(newCell));
 
     if (!side) {
         if (isValidMoveWhite(x_coordinate, y_coordinate, oldCell, newCell)) return true;
@@ -141,6 +151,17 @@ function isValidMove(oldCell, newCell, side) {
  */
 function isValidMoveWhite(x_coordinate, y_coordinate, pieceSelected, newCell) {
     if (pieceSelected.innerHTML.charAt(76) != 'w') return false;
+    if (captureAvaibality) {
+        if (captureCoordinate.includes(newCell.id)) {
+            console.log(x_coordinate[1] + " " + y_coordinate[1]);
+            let temp = (parseInt(newCell.id.substring(0,1)) + x_coordinate[1]) / 2;
+            let temp2 = (parseInt(newCell.id.substring(2, 3)) + y_coordinate[1]) / 2;
+            capture(document.getElementById(temp + "_" + temp2));
+            captureCoordinate = [];
+            captureAvaibality = false;
+            return true;
+        }
+    }
     if (x_coordinate[0] != x_coordinate[1]  &&  y_coordinate[0] - y_coordinate[1] == 1 && newCell.innerHTML == "") {
         return true;
     }
@@ -156,19 +177,109 @@ function isValidMoveWhite(x_coordinate, y_coordinate, pieceSelected, newCell) {
  */
 function isValidMoveBlack(x_coordinate, y_coordinate, pieceSelected, newCell) {
     if (pieceSelected.innerHTML.charAt(76) != 'b') return false;
-    if (x_coordinate[0] != x_coordinate[1]  &&  y_coordinate[0] - y_coordinate[1] == -1 && newCell.innerHTML == "") {
+    // canCapture(newCell);
+    if (captureAvaibality) {
+        if (captureCoordinate.includes(newCell.id)) {
+            console.log(x_coordinate[1] + " " + y_coordinate[1]);
+            let temp = (parseInt(newCell.id.substring(0,1)) + x_coordinate[1]) / 2;
+            let temp2 = (parseInt(newCell.id.substring(2, 3)) + y_coordinate[1]) / 2;
+            capture(document.getElementById(temp + "_" + temp2));
+            captureCoordinate = [];
+            captureAvaibality = false;
+            return true;
+        }
+    }
+    if (x_coordinate[0] != x_coordinate[1] && y_coordinate[0] - y_coordinate[1] == -1 && newCell.innerHTML == "") {
         return true;
     }
     return false;
 }
 
 /**
- * X captures Y
+ * The piece getting captured get obliterated from the board
  * @param {*} x 
- * @param {*} y 
  */
 function capture(x) {
-    //TODO
+    x.innerHTML = "";
+}
+
+function canCapture(pieceSelected) {
+    if (!isTherePieceAround(pieceSelected)) return;
+    // let x = parseInt(pieceSelected.id.substring(0, 1));
+    // let y = parseInt(pieceSelected.id.substring(2,3));
+    // console.log(leftBelow(pieceSelected).innerHTML);
+    if (side && (rightBelow(pieceSelected).charAt(76) == "w" || leftBelow(pieceSelected).charAt(76) == 'w' || rightAhead(pieceSelected).charAt(76) == 'w' || leftAhead(pieceSelected).charAt(76) == 'w')) {
+        if (leftBelow(pieceSelected).charAt(76) == 'w') {
+            let enemyTile = document.getElementById(leftBelow(pieceSelected).substring(65, 67)).parentNode;
+
+            let xEnemyTile = parseInt(enemyTile.id.substring(0, 1)) + 2;
+            let yEnemyTile = parseInt(enemyTile.id.substring(2,3)) + 2;
+            console.log(enemyTile);
+            let checkAvailability = document.getElementById(xEnemyTile + "_" + yEnemyTile); 
+            console.log(checkAvailability.innerHTML);
+            if (checkAvailability.innerHTML == "") {
+                captureAvaibality = true;
+                // mustMovePiece = enemyTile;
+                captureCoordinate.push(checkAvailability.id);
+            }
+        }
+        if (rightBelow(pieceSelected).charAt(76) == 'w') {
+            let enemyTile = document.getElementById(rightBelow(pieceSelected).substring(65, 67)).parentNode;
+
+            let xEnemyTile = parseInt(enemyTile.id.substring(0, 1)) - 2;
+            let yEnemyTile = parseInt(enemyTile.id.substring(2,3)) + 2;
+
+            let checkAvailability = document.getElementById(xEnemyTile + "_" + yEnemyTile); 
+            if (checkAvailability.innerHTML == "") {
+                captureAvaibality = true;
+                // mustMovePiece = enemyTile;
+                captureCoordinate.push(checkAvailability.id);
+            }
+        }
+        // if (rightBelow(pieceSelected).charAt(76) == 'w') {
+        //     let enemyTile = document.getElementById(rigthBelow(pieceSelected).substring(65, 67)).parentNode;
+
+        //     let xEnemyTile = parseInt(enemyTile.id.substring(0, 1)) - 2;
+        //     let yEnemyTile = parseInt(enemyTile.id.substring(2,3)) + 2;
+
+        //     let checkAvailability = document.getElementById(xEnemyTile + "_" + yEnemyTile); 
+        //     if (checkAvailability.innerHTML == "") {
+        //         captureAvaibality = true;
+        //         mustMovePiece = enemyTile;
+        //         captureCoordinate.push(checkAvailability.id);
+        //     }
+        // }
+        // console.log("YES");
+    } else if (!side && (rightBelow(pieceSelected).charAt(76) == "b" || leftBelow(pieceSelected).charAt(76) == 'b' || rightAhead(pieceSelected).charAt(76) == 'b' || leftAhead(pieceSelected).charAt(76) == 'b')) {
+        if (rightAhead(pieceSelected).charAt(76) == 'b') {
+            let enemyTile = document.getElementById(rightAhead(pieceSelected).substring(65, 67)).parentNode;
+
+            let xEnemyTile = parseInt(enemyTile.id.substring(0, 1)) - 2;
+            let yEnemyTile = parseInt(enemyTile.id.substring(2,3)) - 2;
+            console.log(enemyTile);
+            let checkAvailability = document.getElementById(xEnemyTile + "_" + yEnemyTile); 
+            console.log(checkAvailability.innerHTML);
+            if (checkAvailability.innerHTML == "") {
+                captureAvaibality = true;
+                // mustMovePiece = enemyTile;
+                captureCoordinate.push(checkAvailability.id);
+            }
+        }
+        if (leftAhead(pieceSelected).charAt(76) == 'b') {
+            let enemyTile = document.getElementById(leftAhead(pieceSelected).substring(65, 67)).parentNode;
+
+            let xEnemyTile = parseInt(enemyTile.id.substring(0, 1)) + 2;
+            let yEnemyTile = parseInt(enemyTile.id.substring(2,3)) - 2;
+
+            let checkAvailability = document.getElementById(xEnemyTile + "_" + yEnemyTile); 
+            if (checkAvailability.innerHTML == "") {
+                captureAvaibality = true;
+                // mustMovePiece = enemyTile;
+                captureCoordinate.push(checkAvailability.id);
+            }
+        }       
+    }
+    
 }
 
 /**
@@ -185,7 +296,10 @@ function swap(sideSwap) {
  * @param {*} selectedPiece 
  * @return true if there are there are, false otherwise
  */
-function lookAround(x, y) {
+function isTherePieceAround(pieceSelected) {
+    let x = parseInt(pieceSelected.id.substring(0, 1));
+    let y = parseInt(pieceSelected.id.substring(2,3));
+
     let xRight = x + 1;
     let xLeft = x - 1;
     let yAhead = y + 1;
@@ -209,6 +323,75 @@ function lookAround(x, y) {
     if(rightAhead != "" || leftAhead != "" || rightBelow != "" || leftBelow != "") return true;
 
     return false;
+}
+
+function rightAhead(pieceSelected) {
+    let x = parseInt(pieceSelected.id.substring(0, 1));
+    let y = parseInt(pieceSelected.id.substring(2,3));
+
+    let xRight = x + 1;
+    let yAhead = y + 1;
+    let rightAhead;
+
+    if (xRight >= 0 && xRight <= 9 && yAhead >=0 && yAhead <= 9)
+    rightAhead = document.getElementById(xRight + "_" + yAhead).innerHTML;
+    else rightAhead = "";
+
+    if (rightAhead != "") {
+        return document.getElementById(xRight + "_" + yAhead).innerHTML;
+    }
+    return "";
+}
+
+function leftAhead(pieceSelected) {
+    let x = parseInt(pieceSelected.id.substring(0, 1));
+    let y = parseInt(pieceSelected.id.substring(2,3));
+
+    let xLeft = x - 1;
+    let yAhead = y + 1;
+
+    let leftAhead;
+    if (xLeft >= 0 && xLeft <= 9 && yAhead >= 0 && yAhead <= 9)
+    leftAhead = document.getElementById(xLeft + "_" + yAhead).innerHTML;
+    else leftAhead = "";
+
+    if (leftAhead != "") return document.getElementById(xLeft + "_" + yAhead).innerHTML;
+
+    return "";
+}
+
+function rightBelow(pieceSelected) {
+    let x = parseInt(pieceSelected.id.substring(0, 1));
+    let y = parseInt(pieceSelected.id.substring(2,3));
+
+    let xRight = x + 1;
+    let yBelow = y - 1;
+
+    let rightBelow;
+
+    if (xRight >= 0 && xRight <= 9 && yBelow >=0 && yBelow <= 9)
+    rightBelow = document.getElementById(xRight + "_" + yBelow).innerHTML;
+    else rightBelow = "";
+
+    if (rightBelow != "") return document.getElementById(xRight + "_" + yBelow).innerHTML;
+
+    return "";
+}
+
+function leftBelow(pieceSelected) {
+    let x = parseInt(pieceSelected.id.substring(0, 1));
+    let y = parseInt(pieceSelected.id.substring(2,3));
+    
+    let xLeft = x - 1;
+    let yBelow = y - 1;
+    let leftBelow;
+    if (xLeft >= 0 && xLeft <= 9 && yBelow >= 0 && yBelow <= 9)
+    leftBelow = document.getElementById(xLeft + "_" + yBelow).innerHTML;
+    else leftBelow = "";
+
+    if(leftBelow != "") return document.getElementById(xLeft + "_" + yBelow).innerHTML;
+
+    return "";
 }
 
 /**
